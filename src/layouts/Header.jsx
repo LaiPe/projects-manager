dc0a98118914dba26cb1f5e7ac6734558db824a0
Léo Peyronnet <peyronnet.leo@gmail.com>
2025-11-09 10:14:55 +0100

    feat(Header): enhance header layout with Bootstrap classes for improved styling and responsiveness

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -5,29 +5,31 @@ function Header() {
     const { isAuthenticated, user } = useAuth();
 
     return (
-        <header>
-            <NavLink to="/">
-                <h1>Projects Manager</h1>
-            </NavLink>
-            <nav>
-                <ul>
-                    {isAuthenticated ? (
-                        // Navigation pour utilisateur connecté
-                        <>
-                            <span>Bienvenue, {user.username}!</span>
-                            <NavLink to="/projects">Projets</NavLink>
-                            <NavLink to="/tasks">Tâches</NavLink>
-                            <NavLink to="/logout">Déconnexion</NavLink>
-                        </>
-                    ) : (
-                        // Navigation pour utilisateur non connecté
-                        <>
-                            <NavLink to="/login">Connexion</NavLink>
-                            <NavLink to="/register">Inscription</NavLink>
-                        </>
-                    )}
-                </ul>
-            </nav>
+        <header className='navbar navbar-expand-lg bg-body-tertiary sticky-top'>
+            <div className='container-fluid d-flex justify-content-between'>
+                <NavLink to="/" className='navbar-brand'>
+                    <h1>Projects Manager</h1>
+                </NavLink>
+                <nav className='collapse navbar-collapse'>
+                    <ul className='navbar-nav ms-auto'>
+                        {isAuthenticated ? (
+                            // Navigation pour utilisateur connecté
+                            <>
+                                <span className='navbar-text me-2'>Bienvenue, {user.username}!</span>
+                                <NavLink to="/projects" className='nav-link'>Projets</NavLink>
+                                <NavLink to="/tasks" className='nav-link'>Tâches</NavLink>
+                                <NavLink to="/logout" className='nav-link'>Déconnexion</NavLink>
+                            </>
+                        ) : (
+                            // Navigation pour utilisateur non connecté
+                            <>
+                                <NavLink to="/login" className='nav-link'>Connexion</NavLink>
+                                <NavLink to="/register" className='nav-link'>Inscription</NavLink>
+                            </>
+                        )}
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
