b6f34b21cbbdca71c2fb9f16caee9c57054ca98d
Léo Peyronnet <peyronnet.leo@gmail.com>
2025-11-09 10:14:55 +0100

    feat(Header): enhance header layout with Bootstrap classes for improved styling and responsiveness

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,33 +1,64 @@
 import { NavLink } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
+import { useState } from 'react';
 
 function Header() {
     const { isAuthenticated, user } = useAuth();
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen);
+    };
 
     return (
-        <header>
-            <NavLink to="/">
-                <h1>Projects Manager</h1>
-            </NavLink>
-            <nav>
-                <ul>
-                    {isAuthenticated ? (
-                        // Navigation pour utilisateur connecté
-                        <>
-                            <span>Bienvenue, {user.username}!</span>
-                            <NavLink to="/projects">Projets</NavLink>
-                            <NavLink to="/tasks">Tâches</NavLink>
-                            <NavLink to="/logout">Déconnexion</NavLink>
-                        </>
-                    ) : (
-                        // Navigation pour utilisateur non connecté
-                        <>
-                            <NavLink to="/login">Connexion</NavLink>
-                            <NavLink to="/register">Inscription</NavLink>
-                        </>
-                    )}
-                </ul>
-            </nav>
+        <header className='navbar navbar-expand-lg bg-body-secondary sticky-top'>
+            <div className='container-fluid'>
+                <NavLink to="/" className='navbar-brand'>
+                    <h1>Projects Manager</h1>
+                </NavLink>
+                
+                {/* Bouton hamburger pour mobile */}
+                <button 
+                    className='navbar-toggler' 
+                    type='button' 
+                    onClick={toggleNav}
+                    aria-controls='navbarNav' 
+                    aria-expanded={isNavOpen} 
+                    aria-label='Toggle navigation'
+                >
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+
+                <nav className={`collapse navbar-collapse ${isNavOpen ? 'show' : ''} `} id='navbarNav'>
+                    <ul className='navbar-nav ms-auto text-end'>
+                        {isAuthenticated ? (
+                            // Navigation pour utilisateur connecté
+                            <>
+                                <span className='navbar-text me-2'>Bienvenue, {user.username}!</span>
+                                <li className='nav-item'>
+                                    <NavLink to="/projects" className='nav-link' onClick={() => setIsNavOpen(false)}>Projets</NavLink>
+                                </li>
+                                <li className='nav-item'>
+                                    <NavLink to="/tasks" className='nav-link' onClick={() => setIsNavOpen(false)}>Tâches</NavLink>
+                                </li>
+                                <li className='nav-item'>
+                                    <NavLink to="/logout" className='nav-link' onClick={() => setIsNavOpen(false)}>Déconnexion</NavLink>
+                                </li>
+                            </>
+                        ) : (
+                            // Navigation pour utilisateur non connecté
+                            <>
+                                <li className='nav-item'>
+                                    <NavLink to="/login" className='nav-link' onClick={() => setIsNavOpen(false)}>Connexion</NavLink>
+                                </li>
+                                <li className='nav-item'>
+                                    <NavLink to="/register" className='nav-link' onClick={() => setIsNavOpen(false)}>Inscription</NavLink>
+                                </li>
+                            </>
+                        )}
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
